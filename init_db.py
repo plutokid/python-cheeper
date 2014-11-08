da5c3d4db641cf37e1c83c74038e2945eb7f6f3f
AnikD <anikdu@gmail.com>
2014-11-07 22:12:07 -0800

    First Commit

---
+++ init_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('cheeps.db')
+
+c = conn.cursor()
+
+c.execute("CREATE TABLE cheeps (name, datetime, cheep)")
+c.execute("INSERT INTO cheeps VALUES ('bob', '100', 'Hello world!')")
+
+c.execute("SELECT * FROM cheeps")
+print(c.fetchall())
+
+conn.commit()
+conn.close()
